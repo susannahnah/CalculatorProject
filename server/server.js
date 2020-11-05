205a6ad527d0e2e4b57ec3ecb6f7652b5ccf6691
Susannah Harris <harris.susannah@gmail.com>
2020-11-04 20:29:25 -0600

    created app.get to check on postman

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+//function - 
+app.get('/', (req, res) => {
+    console.log("testytesttest");
+    res.sendStatus(200);
+})
 
 
 
