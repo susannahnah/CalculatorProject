c92d8ae1dedb70ca97f654a64b8efddcda2d8d79
Susannah Harris <harris.susannah@gmail.com>
2020-11-04 21:09:20 -0600

    create math equation module

---
+++ server/modules/mathequations.js
@@ -0,0 +1,26 @@
+//abstraction of math equations
+// create math functions using x, y, and operator variables
+const tacoMath = function(x, y, Operator) {
+    
+    //create variable that will be returned at end function
+    let result;
+    
+    //create math operations/string recognition 
+    if (operator === "+") {
+        result = Number(x) + Number(y); 
+    }
+    else if(operator === "-") {
+        result = Number(x) - Number(y);
+    }
+    else if(operator === "*") {
+        result = Number(x) * Number(y);
+    }
+    else (
+        result = Number(x) / Number(y)
+    )
+    return result; 
+
+}
+
+
+module.exports = tacoMath; 
