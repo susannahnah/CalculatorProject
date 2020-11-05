39804130c28f8cd17d764580a63868f90c02410c
Susannah Harris <harris.susannah@gmail.com>
2020-11-04 20:22:47 -0600

    create node server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const mathymath = require("./modules/mathymath");
 
 
-app.use(express.static('server/public'));
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
-const mathAnswer = require('./modules/mathanswers');
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Up and running on PORT: ${PORT}`);
+});
