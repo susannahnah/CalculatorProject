a617e830750ef3a181cd17ab547c8d972c827981
Susannah Harris <harris.susannah@gmail.com>
2020-11-04 22:59:45 -0600

    setting up socket get and post

--- server/server.js
+++ server/server.js
@@ -1,24 +1,48 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const mathequations = require("./modules/mathequations");
+
+//where math answers are stored on the server side
+const answerHistory = [1, 2];
 
 //creating server and defining port
-const app = express();
 const PORT = process.env.PORT || 5000;
-
-//giving access to req.body sent from front end to serverside
-app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(express.json());
 
 
 //function - creating "GET" route
 app.get('/', (req, res) => {
-    console.log("testytesttest");
-    res.sendStatus(200);
-})
+    res.sendFile(__dirname + '/public/index.html');
+});
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    //emitting to 'calculation', only listens to 'calculation' from front end
+    //once it's heard, will do newCalc function
+    socket.on('calculation', (newCalc) => {
+
+        const { numOne, numTwo, operator }  = newCalc;
+        const answer = mathequations(numOne, numTwo, operator);
+        answerHistory.push(answer);
+        console.log(answerHistory);
+
+        io.emit('new calculation', answerHistory);
+    });
+
+});
+
+//function - post new equations to server
 app.post('/newequation', (req, res) => {
+
     console.log(req.body);
-    res.sendStatus(200);
+    const { numOne, numTwo, operator }  = req.body;
+    const answer = mathequations(numOne, numTwo, operator);
+    answerHistory.push(answer);
+
+    //answer from two variables put into answerHistory and sent back to front end
+    res.status(200).send(answerHistory); 
 })
 
 
@@ -26,6 +50,6 @@ app.post('/newequation', (req, res) => {
 
 
 //starting up server
-app.listen(PORT, () => {
+http.listen(PORT, () => {
     console.log(`Up and running on PORT: ${PORT}`);
 });
